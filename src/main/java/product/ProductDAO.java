b461c898de935bd35c9095561787eb8bd86ef4e5
wony9111 <81408036+wony9111@users.noreply.github.com>
2021-05-01 15:43:06 +0900

    Update ProductDAO.java

--- src/main/java/product/ProductDAO.java
+++ src/main/java/product/ProductDAO.java
@@ -18,7 +18,7 @@ public class ProductDAO {
 		
 		try {
 			conn = DBConnection.getConnection();
-			String sql = "INSERT INTO PRODUCT(product_name, user_id, category_no, min_price, max_price, current_price, regist_date, end_date, product_discription,thumnail,img1,img2,state) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)";
+			String sql = "INSERT INTO PRODUCT(product_name, user_id, category_no, min_price, max_price, current_price, regist_date, end_date, product_discription,thumnail,img1,img2) VALUES(?,?,?,?,?,?,?,?,?,?,?,?)";
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, product_name);
 			pstmt.setString(2, user_id);
@@ -32,7 +32,6 @@ public class ProductDAO {
 			pstmt.setString(10, filename1);
 			pstmt.setString(11, filename2);
 			pstmt.setString(12, filename3);
-			pstmt.setString(13, "경매전");
 			pstmt.executeUpdate();
 			result = true;
 		}catch (Exception e) {
@@ -48,12 +47,11 @@ public class ProductDAO {
 		return result;
 	}
 	
-	//카테고리별
+	//�긽�뭹媛��졇�삤湲�
 	public List<ProductDTO> findProducts(int category_no){
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		List<ProductDTO> productyList = new ArrayList<>(); 
-		
 		try {
 			conn = DBConnection.getConnection();
 			String sql = "SELECT * FROM product WHERE category_no=? ORDER BY regist_date DESC";
@@ -89,6 +87,48 @@ public class ProductDAO {
 			}
 		}
 		
+		return productyList;
+	}
+	
+	public List<ProductDTO> findProducts(int category_no,String keyword){
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		List<ProductDTO> productyList = new ArrayList<>(); 
+		try {
+			conn = DBConnection.getConnection();
+			String sql = "SELECT * FROM product WHERE category_no=? AND product_name LIKE ? ORDER BY regist_date DESC";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, category_no);
+			pstmt.setString(2, "%"+keyword+"%");
+			ResultSet rs = pstmt.executeQuery();
+			while(rs.next()) {
+				ProductDTO dto = new ProductDTO();
+				dto.setProduct_code(rs.getInt("product_code"));
+				dto.setProduct_name(rs.getString("product_name"));
+				dto.setUser_id(rs.getString("user_id"));
+				dto.setCategory_no(rs.getInt("category_no"));
+				dto.setMin_price(rs.getInt("min_price"));
+				dto.setMax_price(rs.getInt("max_price"));
+				dto.setCurrent_price(rs.getInt("current_price"));
+				dto.setRegist_date(rs.getString("regist_date"));
+				dto.setEnd_date(rs.getString("end_date"));
+				dto.setDiscription(rs.getString("product_discription"));
+				dto.setThumnail(rs.getString("thumnail"));
+				dto.setImg1(rs.getString("img1"));
+				dto.setImg2(rs.getString("img2"));
+
+				productyList.add(dto);
+			}
+		}catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			try{
+				if ( pstmt != null ){ pstmt.close(); pstmt=null; }
+				if ( conn != null ){ conn.close(); conn=null;    }
+			}catch(Exception e){
+				throw new RuntimeException(e.getMessage());
+			}
+		}
 		return productyList;
 	}
 	
@@ -160,8 +200,6 @@ public class ProductDAO {
 				dto.setThumnail(rs.getString("thumnail"));
 				dto.setImg1(rs.getString("img1"));
 				dto.setImg2(rs.getString("img2"));
-				dto.setBidder(rs.getString("bidder"));
-				dto.setState(rs.getString("state"));
 			}
 		}catch (Exception e) {
 			e.printStackTrace();
@@ -174,30 +212,6 @@ public class ProductDAO {
 			}
 		}
 		return dto;
-	}
-	
-	//상품삭제 - 해야해
-	public int deleteProduct(int product_code) {
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		int result = -1;
-		try {
-			conn = DBConnection.getConnection();
-			String sql = "DELETE FROM product WHERE product_code=? and state='경매전' or state='경매종료'";
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setInt(1, product_code);
-			result = pstmt.executeUpdate();	//성공하면 1
-		}catch (Exception e) {
-			e.printStackTrace();
-		}finally {
-			try{
-				if ( pstmt != null ){ pstmt.close(); pstmt=null; }
-				if ( conn != null ){ conn.close(); conn=null;    }
-			}catch(Exception e){
-				throw new RuntimeException(e.getMessage());
-			}
-		}
-		return result;
 	}
 	
 }
